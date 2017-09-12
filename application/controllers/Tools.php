40cc13a0aaa5aff9009d24d95531a1b018f65499
dimas yusuf <dimas.yusuf@polkam.go.id>
2017-09-12 20:02:33 +0700

    Implement cron method to update status of those overdue project

--- application/controllers/Tools.php
+++ application/controllers/Tools.php
@@ -2,46 +2,19 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
+class Tools extends CI_Controller {
 
     function __construct() {
-        parent::__construct();		
-        $this->load->library('access');
+        parent::__construct();
     }
 
-    function index() {
-        $this->access->logout();
-        $this->login();
-    }
-
-    function login() {
-        $this->load->library('form_validation');
-
-        $this->form_validation->set_rules('email', 'Username', 'trim|required|strip_tags');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required');
-        $this->form_validation->set_rules('token', 'token', 'callback_check_login');
-        if ($this->form_validation->run() == false) {
-            $this->load->view('login');
-        } else {
-              redirect('dashboard');
-        }
-    }
-
-    function logout() {
-        $this->access->logout();
-        redirect();
-    }
-
-    function check_login() {
-        $username = $this->input->post('email', true);
-        $password = $this->input->post('password', true);
-
-        $login = $this->access->login($username, $password);
-        if ($login) {
-            return true;
-        } else {
-            $this->form_validation->set_message('check_login', 'Wrong username or password');
-            return false;
+    /**
+     * Sebenarnya ini buat ganti status project di tengah malam
+     * @param type $to
+     */
+    public function message() {
+        if (is_cli()) {
+            $this->db->query("update `projects` set project_status=3 WHERE date(now())>end_date and project_status=1");
         }
     }
 
