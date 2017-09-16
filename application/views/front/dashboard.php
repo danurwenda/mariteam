4e953b656ced239eab457e73562762f2b0e77dd8
dimas yusuf <dimas.yusuf@polkam.go.id>
2017-09-17 04:28:23 +0700

    redraw piechart on window resize in admin panel

--- application/views/front/dashboard.php
+++ application/views/front/dashboard.php
@@ -3,18 +3,9 @@
 <?php echo css_asset('fullcalendar/fullcalendar.print.min.css', null, ['media' => 'print']); ?>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
-    function loadJS() {
-        var script = document.createElement('script');
-        script.src = '<?php echo base_url('dist/js/dashboard.js') ?>';
-        script.async = false;// <-- important
-        document.head.appendChild(script);
-    }
-    google.charts.load('current', {packages: ['corechart']});
-    google.charts.setOnLoadCallback(loadJS)
-</script>
-<script type="text/javascript">
     $$.push(
-            '<?php echo js_asset_url('fullcalendar/fullcalendar.min.js') ?>',
+            '<?php echo js_asset_url('fullcalendar/fullcalendar.min.js') ?>'
+             ,'<?php echo base_url('dist/js/dashboard.js') ?>'
             );
 </script>
 <style>
--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -1,31 +1,44 @@
 $(document).ready(function () {
-    function drawChart(raw) {
-        var data = google.visualization.arrayToDataTable(raw);
+    google.charts.load('current', {packages: ['corechart']});
+    google.charts.setOnLoadCallback(drawPie)
 
-        var options = {
-            colors: ["#5cb85c", "#5bc0de", "#d9534f", "#f9c154", "#1aadce", "#492970", "#f28f43", "#77a1e5", "#c42525", "#a6c96a"],
-            is3D: true,
-            title: 'Project by Status'
-        };
+    $(window).resize(function () {
+        drawPie();
+    });
+    var sraw = [
+        ['Status', '#Projects']
+    ], options = {
+        colors: ["#5cb85c", "#5bc0de", "#d9534f", "#f9c154", "#1aadce", "#492970", "#f28f43", "#77a1e5", "#c42525", "#a6c96a"],
+        is3D: true,
+        title: 'Project by Status'
+    };
+
+    function doDraw(arr) {
+        var data = google.visualization.arrayToDataTable(arr);
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
         chart.draw(data, options);
     }
-    //load statistic
-    $.getJSON(base_url + 'project/get_stats', function (data) {
-        var sraw = [
-            ['Status', '#Projects']
-        ];
 
-        data.forEach(function (stat) {
-            sraw.push([stat.name, Number(stat.total)])
-        })
-        //use it to draw chart
-        drawChart(sraw);
-    })
+    function drawPie() {
+        //load statistic only once
+        if (sraw.length === 1) {
+            //load json
+            $.getJSON(base_url + 'project/get_stats', function (data) {
+
+                data.forEach(function (stat) {
+                    sraw.push([stat.name.substring(7), Number(stat.total)])
+                })
+
+                doDraw(sraw);
+            })
+        } else {
+            //else, draw
+            doDraw(sraw);
+        }
+    }
     //init calendar
-
     $('#calendar').fullCalendar({
         header: {
             left: 'prev,next today',
@@ -36,7 +49,7 @@ $(document).ready(function () {
         eventLimit: true, // allow "more" link when too many events
         events: base_url + 'publik/calendar',
         eventClick: function (e) {
-            window.location = base_url+'event/edit/'+e.id;
+            window.location = base_url + 'event/edit/' + e.id;
         }
     });
 })
