e2be2d9e27b4b59391d54a99b5da420aac90fbde
Ddanurwenda <dimas@danurwenda.com>
2018-07-03 13:57:44 +0700

    cleaning up template

--- application/views/public/template.php
+++ application/views/public/template.php
@@ -27,65 +27,17 @@
 
         <div class="loader"></div>
         <div id="myDiv">
-            <!--HEADER-->
-<!--            <div class="header">
-                <div class="bg-color">
-                    <header id="main-header">
-                        <nav class="navbar navbar-default navbar-fixed-top">
-                            <div class="container">
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                    </button>
-                                    <a class="navbar-brand" href="<?php echo site_url(); ?>">
-                                        Mari<span class="logo-dec">team</span>
-                                        <div style="clear:both"></div>
-                                        <span class="subs">Maritim Project Management Tools</span>
-                                    </a>
-                                </div>
-                                <div class="collapse navbar-collapse" id="myNavbar">
-                                    <ul class="nav navbar-nav navbar-right">
-                                        <li class="<?php echo $page == 'home' ? 'active' : ''; ?>"><a href="<?php echo site_url(); ?>">Home</a></li>
-                                        <li class="<?php echo $page == 'projects' ? 'active' : ''; ?>"><a href="<?php echo site_url('publik/projects'); ?>">Projects</a></li>
-                                        <li class="<?php echo $page == 'events' ? 'active' : ''; ?>"><a href="<?php echo site_url('publik/events'); ?>">Events</a></li>
-                                        <li><a href="<?php echo site_url('dashboard'); ?>">Login</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </header>
-                </div>
-            </div>-->
-            <!--/ HEADER-->
+        
             <!-- Page Content -->
             <section id="content" class="wow fadeIn delay-05s animated" style="visibility: visible; animation-name: fadeIn;">
-                <div class=""><!-- don't set this div with .container class since it will be loaded inside an iframe-->
+                <div><!-- don't set this div with .container class since it will be loaded inside an iframe-->
                     <div class="row">
                         <?php echo $_content; ?>
                     </div>
                 </div>
             </section>
-            <!-- /#page-wrapper -->
-            <!---->
-<!--            <footer id="footer">
-                <div class="container">
-                    <div class="row text-center">
-                        <p>&copy; 2017 Baker Theme & Dimas Danurwenda. All Rights Reserved.</p>
-                        <div class="credits">
-                             
-                                All the links in the footer should remain intact. 
-                                You can delete the links only if you purchased the pro version.
-                                Licensing information: https://bootstrapmade.com/license/
-                                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Baker
-                            
-                            Designed  by <a href="https://bootstrapmade.com/">Bootstrap Themes</a>
-                        </div>
-                    </div>
-                </div>
-            </footer>-->
-            <!---->
+            <!-- /#content -->
+           
         </div>
         <!-- jQuery -->
         <?php echo js_asset('jquery/jquery.min.js'); ?>
